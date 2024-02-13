9ecceb9100b2d4b3950c3edae0c624e1973877d0
Bruno Almeida <brunom.almeida@hotmail.com>
2024-02-13 13:18:54 -0500

    add bash and DB file

--- element.sh
+++ element.sh
@@ -12,10 +12,15 @@ else
     else
         ELEMENT_SELECTED=$($PSQL "SELECT * FROM elements LEFT JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE atomic_number='$1'")
     fi
-    
-    echo "$ELEMENT_SELECTED" | while read TYPE_ID BAR ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT REST
+
+    if [[ -z $ELEMENT_SELECTED ]]
+    then
+        echo "I could not find that element in the database."
+    else
+        echo "$ELEMENT_SELECTED" | while read TYPE_ID BAR ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE
     do
         echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
     done
+    fi
 fi
 
